3226df29162ae5b03c4e343d57adbff7169fbea8
Prasanjit Dey <prasanjitdey8523@gmail.com>
2024-12-28 19:36:16 +0530

    Setup server with dotenv and chat API routes

--- server/server.js
+++ server/server.js
@@ -1,9 +1,26 @@
-const express = require ("express")
+const dotenv = require ("dotenv")
+const express = require ("express");
+const chats  = require("./data/data");
+
 
 const app = express();
-const PORT = 2000;
+dotenv.config()
+const PORT = process.env.PORT || 5000
 
+app.get('/', (req, res) => {
+    res.send("API is running")
+});
+
+app.get('/api/chat', (req, res) => {
+    res.json(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+    // console.log(req.params.id)
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
+})
 
 app.listen(PORT, () => {
-    console.log(`Server started on ${PORT}`)
+    console.log(`Server started on PORT ${PORT}`)
 })

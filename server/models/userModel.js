5aaf6b8a5f0b5d521847a355b3f9a3a7a1d6eb67
Prasanjit Dey <prasanjitdey8523@gmail.com>
2024-12-30 00:30:14 +0530

    feat(user): update User model with additional fields

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -18,7 +18,7 @@ const userSchema = mongoose.Schema (
             type: String,
             require: true,
             default: 
-            ""
+            "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"
         },
     },
     {

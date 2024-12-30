1f991ed0af7cb24c785e3d201a596e8700f05409
Prasanjit Dey <prasanjitdey8523@gmail.com>
2024-12-30 23:55:28 +0530

    feat: add custom color palette to Tailwind configuration

---
+++ client/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        black: '#000000',
+        'deep-black': '#000005',
+        'dark-gray': '#111111',
+        'gray-1': '#2f2f2f',
+        'gray-2': '#3e3e3e',
+        'sky-blue': '#6dc2ff',
+        'mint-green': '#8ef092',
+        'light-gray': '#d9d9d9',
+        lavender: '#e1a3ff',
+        beige: '#f4f2ed',
+        'off-white': '#fdfdfd',
+        white: '#ffffff',
+      },
+    },
+  },
+  plugins: [],
+}
+
